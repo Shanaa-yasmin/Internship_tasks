dbb9e6656181fb3ef4340c0c6aff7301a875cd4e
Shanaa-yasmin <shanshana.kt@gmail.com>
2026-04-02 14:42:46 +0530

    Changes in database

--- WEEK8/core/settings.py
+++ WEEK8/core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-@zp1$6rp913!q707a#awbrfu#bks$y1mvhnz*23&lop^am0po=
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -56,10 +56,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'notes_db'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
